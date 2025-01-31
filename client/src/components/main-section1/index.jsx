a325a120d974f61f0d432d7c105b7391de1903bf
Samra <samrazafar003@gmail.com>
2025-01-31 20:58:50 +0500

    get started button fixed

--- client/src/components/main-section1/index.jsx
+++ client/src/components/main-section1/index.jsx
@@ -1,39 +1,42 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";  // Import the useNavigate hook
 import "./App.css";
 import Robo from "./Robo-3d";
 
 const Section1 = () => {
+  const navigate = useNavigate(); // Initialize the navigate function
+
+  const handleGetStartedClick = () => {
+    navigate("/publish"); // Navigate to the /publish page when the button is clicked
+  };
+
   return (
     <>
-    
-    <div className="section1-bg w-full h-full flex flex-col md:flex-row justify-between px-6 md:px-20 mb-20">
-      
-      <div className="flex flex-col items-center md:items-start md:ml-16">
-        <h1 className="text-2xl md:text-4xl font-bold mt-4 md:mt-24">
-          HI BOT,
-        </h1>
-        
-        <h1 className="text-2xl md:text:block md:text-4xl font-bold mt-4 text-black">
-          TECH MAGAZINE WITH THE COOLEST IDEAS. JOIN THE NEXT-LEVEL TECH, FROM QUANTUM COMPUTING TO AI. MIND-BOGGLING IDEAS TO GET YOU OUT OF YOUR TECH SLUMP!
-        </h1>
-        <h1 className="text-2xl md:text-4xl font-bold mt-4 text-black">
-          HAVE SOMETHING AMAZING TO SHARE?
-        </h1>
-        <h1 className="text-xl md:text-2xl font-bold mt-2 font-color-1">
-          WE WOULD LOVE TO HEAR FROM YOU!
-        </h1>
-        <button
-          className="w-full md:w-1/4 p-3 rounded-3xl mt-4 border-2 border-black"
-          style={{ backgroundColor: "#00E0EE" }}
-        >
-          GET STARTED
-        </button>
+      <div className="section1-bg w-full h-full flex flex-col md:flex-row justify-between px-6 md:px-20 mb-20">
+        <div className="flex flex-col items-center md:items-start md:ml-16">
+          <h1 className="text-2xl md:text-4xl font-bold mt-4 md:mt-24">
+            HI BOT,
+          </h1>
+
+          <h1 className="text-2xl md:text:block md:text-4xl font-bold mt-4 text-black">
+            TECH MAGAZINE WITH THE COOLEST IDEAS. JOIN THE NEXT-LEVEL TECH, FROM QUANTUM COMPUTING TO AI. MIND-BOGGLING IDEAS TO GET YOU OUT OF YOUR TECH SLUMP!
+          </h1>
+          <h1 className="text-2xl md:text-4xl font-bold mt-4 text-black">
+            HAVE SOMETHING AMAZING TO SHARE?
+          </h1>
+          <h1 className="text-xl md:text-2xl font-bold mt-2 font-color-1">
+            WE WOULD LOVE TO HEAR FROM YOU!
+          </h1>
+          
+          <button
+            onClick={handleGetStartedClick}  // Call handleGetStartedClick on button click
+            className="w-full md:w-1/4 p-3 rounded-3xl mt-4 border-2 border-black"
+            style={{ backgroundColor: "#00E0EE" }}
+          >
+            GET STARTED
+          </button>
+        </div>
       </div>
-      
-     
-      
-    </div>
-   
     </>
   );
 };
